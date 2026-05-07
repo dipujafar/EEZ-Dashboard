e4e07995ab2bd955d3f91820350c31916e09f3ad
dipujafar <dipujafarmr@gmail.com>
2026-05-07 12:43:42 +0600

    change

--- src/app/(adminDashboard)/manage-content-type/_components/RichTextEditor.tsx
+++ src/app/(adminDashboard)/manage-content-type/_components/RichTextEditor.tsx
@@ -2,6 +2,7 @@ import dynamic from 'next/dynamic';
 import { useRef } from 'react';
 
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+// @ts-ignore
 import 'react-quill/dist/quill.snow.css';
 
 interface RichTextEditorProps {
@@ -16,24 +17,33 @@ const RichTextEditor = ({ value, onChange, placeholder }: RichTextEditorProps) =
   const modules = {
     toolbar: [
       ['image'],
-      [{ 'size': ['small', false, 'large'] }],
-      ['bold', 'italic', 'underline'],
-      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      [{ 'indent': '-1'}, { 'indent': '+1' }],
-      ['link'],
+      [{
+        'size': ['8px', '10px', '12px', '14px', '16px', '18px', '20px', '24px', '28px', '32px', '36px', '48px', '72px']
+      }],
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ 'script': 'sub' }, { 'script': 'super' }],
+      [
+        { 'list': 'ordered' },   // 1. 2. 3.
+        { 'list': 'bullet' },    // • bullet
+        { 'list': 'check' },     // ☑ checklist
+      ],
+      [{ 'indent': '-1' }, { 'indent': '+1' }],  // indented sub-lists
+      [{ 'align': [] }],
+      [{ 'color': [] }, { 'background': [] }],
+      ['link', 'blockquote', 'code-block'],
       ['clean']
     ],
   };
 
   const formats = [
-   'image' ,'size', 'bold', 'italic', 'underline', 
+    'image', 'size', 'bold', 'italic', 'underline',
     'list', 'bullet', 'indent', 'link'
   ];
 
   return (
     <div className="bg-white rounded-lg border">
       <ReactQuill
-      // @ts-ignore
+        // @ts-ignore
         ref={quillRef}
         theme="snow"
         value={value}
